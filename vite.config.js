fa1265732c5fde98682fb7ab078c7d4a84c5e7d1
Minidu Oshan <126508461+MiniduOshan@users.noreply.github.com>
2026-02-08 20:37:56 +0530

    base config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Aurion-Official-web-site/',
+  base: '/',
   plugins: [react()],
 })
